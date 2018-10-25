7b45cabf78d87aa63b8cec3dc4b217e166e68098
Dorian bourgeoisat <dorian.bourgeoisat@icloud.com>
2018-10-25 18:57:25 +0200

    Loads of changes
    -Added some resources
    -Added card structure
    -Added attack structure

---
+++ Assets/Resources/Scripts/Card.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public enum Type
+{
+    None,
+    Normal,
+    Psy,
+    Dark,
+    Dragon,
+    Fighting,
+    Ghost,
+    Flying,
+    Poison,
+    Ground,
+    Rock,
+    Bug,
+    Steel,
+    Fire,
+    Water,
+    Grass,
+    Electric,
+    Ice,
+    Fairy
+}
+
+[System.Serializable]
+public class Stats
+{
+    public int HP;
+    public int Att;
+    public int Def;
+    public int SpAtt;
+    public int SpDef;
+    public int Speed;
+}
+
+public class Card : MonoBehaviour {
+    public string cardName;
+    public Stats stats;
+    public Type type1;
+    public Type type2;
+    public int attack1ID;
+    public int attack2ID;
+    public int rank;
+    public Status status;
+    public Transform nameObject;
+    public Transform attack1Object;
+    public Transform attack2Object;
+    public Transform rankObject;
+    public Transform type1Object;
+    public Transform type2Object;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Resources/Scripts/Game.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public enum Status
+{
+    NONE, PARALYSED, SLEEPING, POISONED, BURNED, FROZEN
+}
+
+[System.Serializable]
+public class Attack
+{
+    public string name;
+    public int defenderPower;
+    public int attackerPower;
+    public Status statusChangeDefender;
+    public Status statusChangeAttacker;
+    public Stats statChangeDefender;
+    public Stats statChangeAttacker;
+}
+
+public class Game : MonoBehaviour {
+    public List<Attack> attacks = new List<Attack>();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
